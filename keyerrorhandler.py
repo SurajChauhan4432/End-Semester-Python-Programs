894a586ec901afe8240272349505b475e6fcce9f
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 12:05:08 +0530

    Add files via upload

---
+++ keyerrorhandler.py
@@ -0,0 +1,15 @@
+data = {
+    "name": "Ben",
+    "age": 30,
+    "city": "New York"
+}
+
+key = "country"
+
+try:
+    value = data[key]
+    print(f"Value for '{key}': {value}")
+except KeyError:
+    print(f"KeyError: The key '{key}' does not exist in the dictionary.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
