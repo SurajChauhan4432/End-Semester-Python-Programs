89d6cf7926977f9dd46c70b4820c8a94c2f343fd
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 19:21:38 +0530

    Delete modulenotfounderror.py

--- modulenotfounderror.py
+++
@@ -1,7 +0,0 @@
-# Make module not found error handler with built in exceptions. 
-try:
-    import magic
-except ModuleNotFoundError:
-    print("ModuleNotFoundError: The module you are trying to import does not exist.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
