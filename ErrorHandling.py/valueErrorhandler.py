4a07a59ac3fe53f5a2fd2c56742b3b69ad15e368
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 19:17:58 +0530

    Create valueErrorhandler.py

---
+++ ErrorHandling.py/valueErrorhandler.py
@@ -0,0 +1,10 @@
+# Make value error handler with built in exceptions. 
+num = input("Enter a number: ")
+
+try:
+    number = int(num)
+    print(f"You entered the number: {number}")
+except ValueError:
+    print(f"ValueError: '{num}' is not a valid integer.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
