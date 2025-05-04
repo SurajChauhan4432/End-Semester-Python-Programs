643f4dc9f99061171611208e0e8a8aed7f214b1d
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 12:18:55 +0530

    Add files via upload

---
+++ attributeErrorhandler.py
@@ -0,0 +1,9 @@
+text = "Hello, world!"
+
+try:
+    result = text.false_method()
+    print(result)
+except AttributeError:
+    print("AttributeError: The object does not have the attribute or method you tried to use.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
