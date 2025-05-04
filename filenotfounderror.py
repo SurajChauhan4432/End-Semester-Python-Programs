578517e8ee54c461106cadfbfaaf3c666f72fb55
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 12:22:35 +0530

    Add files via upload

---
+++ filenotfounderror.py
@@ -0,0 +1,10 @@
+file_name = "non_existing_file.txt"
+
+try:
+    with open(file_name, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print(f"FileNotFoundError: The file '{file_name}' was not found.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
