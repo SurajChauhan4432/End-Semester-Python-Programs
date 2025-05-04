f1c933f94f7a4ab298f8f2bc68117ed33a0a9cd3
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 15:53:15 +0530

    Create dict3.py

---
+++ Modulequestion1.py/dict3.py
@@ -0,0 +1,14 @@
+my_dict = {}
+
+def len3():
+    return len(my_dict)
+
+def add3(k, v):
+    my_dict[k] = v
+    return my_dict
+
+def keys3():
+    return list(my_dict.keys())
+
+def values3():
+    return list(my_dict.values())
