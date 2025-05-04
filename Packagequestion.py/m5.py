d449b01d279cf582df79ffe2a289d67ec8d6f7ac
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 16:23:33 +0530

    Create m5.py

---
+++ Packagequestion.py/m5.py
@@ -0,0 +1,18 @@
+from mypackage import list1, set2, dict3
+
+# List operations
+print("List append:", list1.append1(10))
+print("List extend:", list1.extend1([20, 30]))
+print("List remove:", list1.remove1(20))
+print("List pop:", list1.pop())
+
+# Set operations
+print("Set add:", set2.adds2("apple"))
+print("Set remove:", set2.remove2("apple"))
+print("Set length:", set2.slen2())
+
+# Dictionary operations
+print("Dict add:", dict3.add3("key", "value"))
+print("Dict keys:", dict3.keys3())
+print("Dict values:", dict3.values3())
+print("Dict length:", dict3.len3())
