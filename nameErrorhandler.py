abfcda16f4778f937ab2dbe0097869d7ce639b41
SurajChauhan4432 <rudraksh246@gmail.com>
2025-05-04 19:21:50 +0530

    Delete nameErrorhandler.py

--- nameErrorhandler.py
+++
@@ -1,9 +0,0 @@
-# Make name error handler with built in exceptions.
-try:
-    print("undefined_variable")
-except NameError:
-    print("Error: You tried to use a variable that hasn't been defined.")
-else:
-    print("Variable printed successfully.")
-finally:
-    print("Name error handling completed.")
